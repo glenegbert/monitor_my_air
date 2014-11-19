14725e05bcbc492076bfbf0f1e17a4b61f043e44
Glen Egbert <glenegbert1@gmail.com>
2014-11-19 13:44:19 -0700

    basic zip code and health concern search working

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,13 +6,7 @@ class ReportsController < ApplicationController
   end
 
   def create
-    @report = Report.new(params[:zip_code])
-    @concerns = checked_concerns(params)
-    # create a method that returns pollutants and values
-    # look at each health concern indicated
-    # check to see if the value for specified polutants is in an orange range
-    # check to see if a value for any pollutant is in a red range
-
+    @report = Report.new(params[:zip_code], params)
   end
 
   def show
@@ -21,8 +15,5 @@ class ReportsController < ApplicationController
 
   private
 
-  def checked_concerns(params)
-    checked_concerns = params.map {|concern, value| concern if value == "1"}.compact
-    checked_concerns.map {|concern| concern.gsub("_", " ")}
-  end
+
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,8 +3,9 @@ require 'date'
 
 class Report
 
-  def initialize(zip_code)
+  def initialize(zip_code, params)
     @zip_code = zip_code
+    @params = params
   end
 
   def forecast
@@ -40,9 +41,78 @@ class Report
     Hash[observed_contaminates.zip observed_levels]
   end
 
+  def observed_aqi_values
+    JSON.parse(observed.body).map {|data| data["AQI"]}
+  end
+
+  def observed_contaminates_and_aqi_values
+    Hash[observed_contaminates.zip observed_aqi_values]
+  end
+
   def reporting_area
     JSON.parse(observed.body)[0]["ReportingArea"]
   end
 
+  def clean_checked_concerns
+    checked_concerns.map {|concern| concern.gsub("_", " ")}
+  end
 
+  def checked_concerns
+    checked_concerns = @params.map {|concern, value| concern if value == "1"}.compact
+  end
+
+  def lung_disease?(day)
+    condition_indicators = ["O3","PM2.5","PM10"]
+    alert?(condition_indicators, day)
+  end
+
+  def heart_disease?(day)
+    condition_indicators = ["PM2.5","PM10"]
+    alert?(condition_indicators, day)
+  end
+
+  def children?(day)
+    condition_indicators = ["O3","PM2.5","PM10"]
+    alert?(condition_indicators, day)
+  end
+
+  def older_adult?(day)
+    condition_indicators = ["O3","PM2.5","PM10"]
+    alert?(condition_indicators, day)
+  end
+
+  def active_outdoors?(day)
+    condition_indicators = ["O3"]
+    alert?(condition_indicators, day)
+  end
+
+  def general_population?(day)
+    red_zone?
+  end
+
+
+  def alert?(condition_indcators, day)
+    if day == "today"
+      contaminates_aqis = observed_contaminates_and_aqi_values
+    else
+      contaminates_aqis = forecasted_contaminates_and_aqi_values
+    end
+    present_condition_indicators = contaminates_aqis.keys.map do |contaminate|
+      if condition_indcators.include?(contaminate)
+        contaminate
+      end
+    end.compact
+    present_condition_indicators.any?{|indicator| observed_contaminates_and_aqi_values[indicator] > 100} ||
+    red_zone?
+  end
+
+  def red_zone?
+    observed_contaminates_and_aqi_values.values.any? {|value| value > 150}
+  end
+
+  def health_effects?(day)
+    check_alerts_methods = checked_concerns.map {|concern| concern + "?"}
+    alerts = check_alerts_methods.map {|method| send(method, day)}
+    alerts.include?(true)
+  end
 end
