35ff686def1bfb67574c76c597b362289406151c
Glen Egbert <glenegbert1@gmail.com>
2014-11-23 11:01:09 -0700

    Notifications Form

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -8,12 +8,21 @@ class NotificationsController < ApplicationController
   end
 
   def create
-    @notification = Notification.create(params)
+    @notification = Notification.create(notification_params)
+    set_conditions(params)
+    redirect_to notifications_path
   end
 
   private
 
-  def notifiation_params
+  def notification_params
+    params.require(:notification).permit(:name, :zip_code, :email, :phone_number)
   end
 
+  def set_conditions(params)
+    condition_ids = params["notification"]["condition_ids"][1..-1]
+    @notification.conditions = condition_ids.map do |id|
+      Condition.find(id)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+conditions =  [ "lung_disease",
+                "heart_disease",
+                "children",
+                "older_adult",
+                "active_outdoors",
+                "general_population"]
+
+conditions.map do |condition|
+  Condition.create(name: condition)
+end
