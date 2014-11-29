fc5abd6bc2ab98a7ef219225dddb7610c4c129f4
Glen Egbert <glenegbert1@gmail.com>
2014-11-29 06:50:17 -0700

    error reporting on reports/create form, reports form partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   respond_to :html, :js
   def index
     @report = Report.new
+    session[:return_to] = "home"
   end
 
 end
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,16 +1,19 @@
 
 class ReportsController < ApplicationController
   respond_to :html, :js
-  
+
   def index
     redirect_to home_index_path
   end
 
   def create
     @report = Report.new(params[:report][:zip_code], params)
-    if @report.invalid?
+    if @report.invalid? && session[:return_to] == "create"
+      render "create"
+    elsif @report.invalid? && session[:return_to] == "home"
       render "home/index"
     end
+    session[:return_to] = "create"
   end
 
   def show
