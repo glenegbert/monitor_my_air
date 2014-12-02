5c5b340e57f0a4f0a39f06bec2aafd7d9ac4875a
Glen Egbert <glenegbert1@gmail.com>
2014-12-02 10:46:44 -0700

    refactored create action in notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -15,14 +15,7 @@ class NotificationsController < ApplicationController
     set_conditions(params)
     @new_notification.user_id = current_user.id
     @new_notification.save
-    if @new_notification.email.length > 0 && @new_notification.phone_number
-      UserNotifier.delay.notification_creation_email(@new_notification)
-      TextSender.delay.send_notification_creation_text(@new_notification)
-    elsif @new_notification.email.length > 0
-      UserNotifier.delay.notification_creation_email(@new_notification)
-    else
-      TextSender.delay.send_notification_creation_text(@new_notification)
-    end
+    send_welcome_messages
     @notification = Notification.new
   end
 
@@ -53,6 +46,17 @@ class NotificationsController < ApplicationController
     condition_ids = params["notification"]["conditions"][1..-1]
     @new_notification.conditions = condition_ids.map do |id|
       Condition.find(id)
+    end
+  end
+
+  def send_welcome_messages
+    if @new_notification.email.length > 0 && @new_notification.phone_number
+      UserNotifier.delay.notification_creation_email(@new_notification)
+      TextSender.delay.send_notification_creation_text(@new_notification)
+    elsif @new_notification.email.length > 0
+      UserNotifier.delay.notification_creation_email(@new_notification)
+    else
+      TextSender.delay.send_notification_creation_text(@new_notification)
     end
   end
 end
